75fafcb7079cc5a531a1fd4f162831cfeb22689b
Marius Appel <marius.appel@uni-muenster.de>
2016-12-09 10:31:11 +0100

    computation of recursive residuals now uses RcppArmadillo

--- R/recresid.R
+++ R/recresid.R
@@ -21,17 +21,44 @@ recresid.lm <- function(x, data = list(), ...)
     return(rr)
 }
 
+
+## convenience function to replace NAs with 0s in coefs
+.coef0 <- function(obj) {
+  cf <- obj$coefficients
+  ifelse(is.na(cf), 0, cf)
+}
+.Xinv0 <- function(obj) {
+  qr <- obj$qr
+  k = length(obj$coefficients)
+  rval <- matrix(0, ncol = k, nrow = k)
+  wi <- qr$pivot[1:qr$rank]
+  rval[wi,wi] <- chol2inv(qr$qr[1:qr$rank, 1:qr$rank, drop = FALSE])
+  rval
+}
+
+
 recresid.default <- function(x, y, start = ncol(x) + 1, end = nrow(x),
   tol = sqrt(.Machine$double.eps)/ncol(x), ...)
 {
   ## checks and data dimensions
   stopifnot(start > ncol(x) & start <= nrow(x))
   stopifnot(end >= start & end <= nrow(x))
+  return(sc_cpp_recresid(x,y,start,end,tol))
+}
+
+
+
+.recresid.default.old <- function(x, y, start = ncol(x) + 1, end = nrow(x),
+                                 tol = sqrt(.Machine$double.eps)/ncol(x), ...)
+{
+  # checks and data dimensions
+  stopifnot(start > ncol(x) & start <= nrow(x))
+  stopifnot(end >= start & end <= nrow(x))
   n <- end
   q <- start - 1
   k <- ncol(x)
   rval <- rep(0, n - q)
-
+  
   ## convenience function to replace NAs with 0s in coefs
   coef0 <- function(obj) {
     cf <- obj$coefficients
@@ -44,47 +71,48 @@ recresid.default <- function(x, y, start = ncol(x) + 1, end = nrow(x),
     rval[wi,wi] <- chol2inv(qr$qr[1:qr$rank, 1:qr$rank, drop = FALSE])
     rval
   }
-
+  
   ## initialize recursion
   y1 <- y[1:q]
   fm <- lm.fit(x[1:q, , drop = FALSE], y1)
-  X1 <- Xinv0(fm)	 
+  X1 <- Xinv0(fm)
   betar <- coef0(fm)
   xr <- as.vector(x[q+1,])
   fr <- as.vector((1 + (t(xr) %*% X1 %*% xr)))
   rval[1] <- (y[q+1] - t(xr) %*% betar)/sqrt(fr)
-
+  
   ## check recursion agains full QR decomposition?
   check <- TRUE
-
+  
   if((q+1) < n)
   {
     for(r in ((q+2):n))
     {
-        ## check for NAs in coefficients
-	nona <- all(!is.na(fm$coefficients))
-    
-	## recursion formula
-        X1 <- X1 - (X1 %*% outer(xr, xr) %*% X1)/fr
-        betar <- betar + X1 %*% xr * rval[r-q-1] * sqrt(fr)
-
-	## full QR decomposition
-	if(check) {
-	  y1 <- y[1:(r-1)]
-	  fm <- lm.fit(x[1:(r-1), , drop = FALSE], y1)
-	  nona <- nona & all(!is.na(betar)) & all(!is.na(fm$coefficients))
-	  ## keep checking?
-	  if(nona && isTRUE(all.equal(as.vector(fm$coefficients), as.vector(betar), tol = tol))) check <- FALSE 
-	  X1 <- Xinv0(fm)
-          betar <- coef0(fm)
+      ## check for NAs in coefficients
+      nona <- all(!is.na(fm$coefficients))
+      
+      ## recursion formula
+      X1 <- X1 - (X1 %*% outer(xr, xr) %*% X1)/fr
+      betar <- betar + X1 %*% xr * rval[r-q-1] * sqrt(fr)
+      
+      ## full QR decomposition
+      if(check) {
+        y1 <- y[1:(r-1)]
+        fm <- lm.fit(x[1:(r-1), , drop = FALSE], y1)
+        nona <- nona & all(!is.na(betar)) & all(!is.na(fm$coefficients))
+        ## keep checking?
+        if(nona && isTRUE(all.equal(as.vector(fm$coefficients), as.vector(betar), tol = tol))) {
+          check <- FALSE
         }
-        
-        ## residual
-        xr <- as.vector(x[r,])
-	fr <- as.vector((1 + (t(xr) %*% X1 %*% xr)))
-	rval[r-q] <- (y[r] - sum(xr * betar, na.rm = TRUE))/sqrt(fr)
+        X1 <- Xinv0(fm)
+        betar <- coef0(fm)
+      }
+      
+      ## residual
+      xr <- as.vector(x[r,])
+      fr <- as.vector((1 + (t(xr) %*% X1 %*% xr)))
+      rval[r-q] <- (y[r] - sum(xr * betar, na.rm = TRUE))/sqrt(fr)
     }
   }
   return(rval)
 }
-
---
+++ src/recresid.cpp
@@ -0,0 +1,111 @@
+#include <RcppArmadillo.h>
+#include <Rcpp.h>
+//[[Rcpp::depends(RcppArmadillo)]]
+  
+#include <iostream>
+  
+using namespace Rcpp;
+
+
+//' Computation of recursive residuals in C++
+//' @param X design matrix
+//' @param y response vector
+//' @param start integer (1-based) index of the first observation to compute recursive residuals
+//' @param end integer (1-based) index of the last observation to compute recursive residuals
+//' @param tol tolerance in the computation of recursive model coefficients
+//' @return vector containing the recursive residuals 
+//' @seealso \code{\link{recresid}} and \code{\link{recresid.default}}
+// [[Rcpp::export]]
+NumericVector sc_cpp_recresid(const arma::mat& X, const arma::vec& y,  unsigned int start, unsigned int end, const double& tol) {
+  
+  if(!(start > X.n_cols && start <= X.n_rows)) stop("Invalid start");
+  if(!(end >= start && end <= X.n_rows)) stop("Invalid end");
+  --start;
+  --end;
+  
+  int n=end; // n is not the number of rows but the last element index, i.e., nrows-1
+  int q=start-1;
+  int k = X.n_cols;
+  arma::vec rval = arma::zeros<arma::vec>(n-q);
+  
+  // If the current submatrix of X has reciprocal conditioning 
+  // number less then the following constant, the ordinary R 
+  // implementation with column-pivoting QR decomposition 
+  // is used. Otherwise, Armadillo functions solve() and 
+  // inv_sympd() are used. This applies only to the first 
+  // iterations as long as check is true. 
+  const double rcond_min = sqrt(DBL_EPSILON);
+  
+  arma::vec cur_y = y.subvec(0, q);
+  arma::mat cur_X = X.submat(0, 0, q, k-1);
+  arma::mat X1;
+  arma::colvec cur_coef_full;
+  
+  Environment env_stats("package:stats");
+  Environment env_sc("package:strucchange");
+  Function fXinv0 =  env_sc[".Xinv0"];
+  Function fcoef0 =  env_sc[".coef0"];
+  Function lmfit = env_stats["lm.fit"];
+  
+  // Decide whether to use Armadillo or R depending on the reciprocal conditioning number of cur_X
+  if (1/arma::cond(cur_X) >= rcond_min) {
+    arma::solve(cur_coef_full, cur_X, cur_y, arma::solve_opts::no_approx);
+    arma::inv_sympd(X1, trans(cur_X) * cur_X);
+  }
+  else {
+    List fitted = lmfit(Named("x", cur_X), Named("y", cur_y));
+    NumericMatrix qrinv = fXinv0(fitted);
+    X1 =  as<arma::mat>(qrinv);
+    cur_coef_full  = as<arma::colvec>(fcoef0(fitted));
+  }
+  
+  arma::colvec cur_coef = cur_coef_full; 
+  arma::mat xr = X.row(q+1); 
+  arma::vec fr = (1 + xr * X1  * trans(xr));
+  rval(0) = as_scalar((y(q+1) - xr * cur_coef)/arma::sqrt(fr));
+  bool check = true;
+  
+  if((q+1) < n)
+  {
+    for(int r=q+2; r<=n; ++r) {
+      X1 -= (X1 * trans(xr) * xr * X1)/as_scalar(fr);
+      cur_coef += X1 * trans(xr) * rval(r-q-2) * sqrt(fr); 
+      
+      if (check) {
+        cur_y = y.subvec(0, r-1);
+        cur_X = X.submat(0, 0, r-1, k-1);
+        
+        // Decide whether to use Armadillo or R depending in the reciprocal conditioning number of cur_X
+        if (1/arma::cond(cur_X) >= rcond_min) {
+          arma::solve(cur_coef_full, cur_X, cur_y, arma::solve_opts::no_approx);
+          arma::inv_sympd(X1, trans(cur_X) * cur_X);
+          bool nona = arma::is_finite( cur_coef) && arma::is_finite( cur_coef_full );
+          if(nona && approx_equal(cur_coef_full,cur_coef, "absdiff", tol)) {
+            check = false;
+          } 
+        }
+        else {
+          List fitted = lmfit(Named("x", cur_X), Named("y", cur_y));
+          NumericMatrix qrinv = fXinv0(fitted);
+          X1 =  as<arma::mat>(qrinv);
+          arma::colvec coef1 = as<arma::colvec>(fitted["coefficients"]);
+          bool nona = arma::is_finite( coef1 ) && arma::is_finite( cur_coef_full );
+          if(nona && approx_equal(coef1,cur_coef, "absdiff", tol)) {
+            check = false;
+          } 
+          cur_coef_full  = as<arma::colvec>(fcoef0(fitted));
+        }
+        
+        cur_coef = cur_coef_full; 
+      }
+      xr = X.row(r); // This a a row 
+      fr = (1 + xr * X1  * trans(xr));
+      rval(r-q-1) = as_scalar((y(r) - xr * cur_coef)/arma::sqrt(fr));
+    }
+  }
+  return  Rcpp::NumericVector(rval.begin(), rval.end());
+}
+
+
+
+
